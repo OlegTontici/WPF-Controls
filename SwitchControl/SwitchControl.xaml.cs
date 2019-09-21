c9422b2358c15706e30ae3f3657a2da412f4972c
DESKTOP-GV0Q4VM\Oleg <olegtontici1993@gmail.com>
2019-09-21 13:26:48 +0300

    Ad switch control

---
+++ SwitchControl/SwitchControl.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace SwitchControl
+{
+    /// <summary>
+    /// Interaction logic for Switch.xaml
+    /// </summary>
+    public partial class SwitchControl : UserControl
+    {
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register("Value", typeof(bool), typeof(SwitchControl), new FrameworkPropertyMetadata { DefaultValue = false, BindsTwoWayByDefault = true });
+
+        public bool Value
+        {
+            get
+            {
+                return (bool)GetValue(ValueProperty);
+            }
+            set
+            {
+                SetValue(ValueProperty, value);
+            }
+        }
+
+        public SwitchControl()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Value = !Value;
+        }
+    }
+}
